aed07b0c39cb21cd78a6f498fcf55a616e7259b3
Robert <robert.starke@mailbox.tu-dresden.de>
2015-05-20 15:26:10 +0200

    Finished stanza chat ability (xmppftw user is broken though (login fails somehow)) but the chat between stanzaio and strophe js users works fine

--- chatapp/js/main.js
+++ chatapp/js/main.js
@@ -1,6 +1,32 @@
-/**
- * Created by rstarke on 11.05.15.
- */
+$(function () {
+
+    var loginBtn = $('.btn-login');
+    var logoutBtn = $('.btn-logout');
+    $('.connected').hide();
+
+
+    /**
+     * Login and Connect
+     */
+    loginBtn.on('click', function (e) {
+        if (e.preventDefault()) {
+            e.preventDefault();
+        }
+        var jid = $('.name').val();
+        var password = $('.password').val();
+        stanzaCreateClient(jid, password);
+    });
+
+    /**
+     * Disconnect and Logout
+     */
+    logoutBtn.on('click', function () {
+        client.disconnect();
+        $('.login').show();
+        $('.connected').hide();
+        clearUserList();
+    });
+});
 
 var users = [
     {"jid": "stanzaio@localhost", local: "stanzaio", "password": "stanza"},
@@ -15,29 +41,70 @@ var fillLogin = function (jid, password) {
     $('.password').val(password);
 };
 
+
 var fillUserList = function (ownJid) {
-    $.each(users, function(index, user){
-        if(ownJid != user.jid){
+    $.each(users, function (index, user) {
+        if (ownJid != user.jid) {
             var buttonGroup = getButtonGroup(user);
             $('.friend-list').append(buttonGroup);
+            var chatWindow = getChatWindow(user);
+            $('.chat-windows').append(chatWindow);
         }
     });
 };
 
-var clearUserList = function(){
-  $('.friend-list').empty();
+var clearUserList = function () {
+    $('.friend-list').empty();
 };
 
-var getButtonGroup = function(user){
-   var buttonGroup =  '<li id="user.local" class="list-group-item clearfix">' + user.local +
-       ' <div class="btn-group user right" role="group">' +
-       '<button type="button" class="btn btn-success btn-open-chat" onclick="openChat(\'' + user.jid + '\');">open chat</button>' +
-       '</div></li>';
+var getButtonGroup = function (user) {
+    var buttonGroup = '<li id="friend-' + user.local + '" class="list-group-item friend clearfix">' + user.local +
+        ' <div class="btn-group user right" role="group">' +
+        '<button type="button" class="btn btn-success btn-open-chat" ' +
+        'onclick="openChat(\'' + user.jid + '\',\'' + user.local + '\')">open chat</button>' +
+        '</div></li>';
 
     return buttonGroup;
 };
 
-var openChat = function(jid){
-    receiver = jid;
-    $('.chat-window .panel-heading').html('Chat with ' + jid);
+var getChatWindow = function (user) {
+    var chatWindow = '<div id="chat-window-' + user.local + '" class="panel panel-default chat-window hidden">' +
+        '<div class="panel-heading">Chat with ' + user.local + '</div>' +
+        '<div class="panel-body chat"><ul class="chat-messages"></ul>' +
+        '</div><div class="panel-footer">' +
+        '<div class="input-group">' +
+        '<input class="form-control custom-control message-text text" placeholder="Your Message"/>' +
+        '<span class="input-group-addon btn btn-default btn-send" ' +
+        'onclick="sendMessage(\'' + user.jid + '\',\'' + user.local + '\')">Send</span></div></div></div>'
+
+    return chatWindow;
+};
+
+var openChat = function (jid, local) {
+    console.log('open Chat: ' + jid + ' ' + local);
+    $('.friend').removeClass('active');
+    $('#friend-' + local).addClass('active');
+    $('.chat-window').addClass('hidden');
+    $('#chat-window-' + local).removeClass('hidden');
+};
+
+var sendMessage = function (jid, local) {
+    var message = $('#chat-window-' + local + ' .message-text').val();
+    stanzaSendMessage(message, jid);
+};
+
+var addChatMessage = function (message, own) {
+    console.log('add Chat Message');
+    console.dir(message);
+    console.log('own Message: ' + own);
+    var windowId;
+    if (own) {
+        windowId = message.to.local;
+        $('#chat-window-' + windowId + ' .chat-messages').append('<li class="chat-message own">' +
+            '<span class="text">' + message.body + '</span></li>');
+    } else {
+        windowId = message.from.local;
+        $('#chat-window-' + windowId + ' .chat-messages').append('<li class="chat-message">' +
+            '<span class="text">' + message.body + '</span></li>');
+    }
 };
--- chatapp/js/xmpp-webdemo-stanza.js
+++ chatapp/js/xmpp-webdemo-stanza.js
@@ -2,125 +2,99 @@
  * Class that serves as api for Stanza IO
  */
 
-$(function () {
+var debugMode = true;
+var client;
 
-    var debugMode = true;
-    true;
-    var client;
-    var loginBtn = $('.btn-login');
-    var logoutBtn = $('.btn-logout');
-    var sendBtn = $('.btn-send');
-    $('.connected').hide();
+var stanzaCreateClient = function (jid, password) {
+    client = XMPP.createClient({
+        jid: jid,
+        password: password,
+        wsURL: 'ws://localhost:5280/xmpp-websocket',
+        transports: ['websocket']
+    });
+    initListeners();
+    client.connect();
+};
+
+var stanzaSendMessage = function (message, receiver) {
+    client.sendMessage({
+        to: receiver,
+        body: message
+    });
+};
+
+function initListeners() {
+    /**
+     * On Connected change Login Bar
+     */
+    client.on('connected', function () {
+        $('.login').hide();
+        $('.connected').show();
+    });
 
     /**
-     * Login and Connect
+     *  Session Started Handling
      */
-    loginBtn.on('click', function (e) {
-        if (e.preventDefault()) {
-            e.preventDefault();
-        }
-        var jid = $('.name').val();
-        client = XMPP.createClient({
-            jid: jid,
-            password: $('.password').val(),
-            wsURL: 'ws://localhost:5280/xmpp-websocket',
-            transports: ['websocket']
-        });
-        initListeners();
-        client.connect();
-    });
-
-    /**
-     * Disconnect and Logout
-     */
-    logoutBtn.on('click', function () {
-        client.disconnect();
-        $('.login').show();
-        $('.connected').hide();
-        clearUserList();
-    });
-
-    sendBtn.on('click', function () {
-        var message = $('.message-text').val();
-        client.sendMessage({
-            to: receiver,
-            body: message
-        });
-    });
-
-    function initListeners() {
-        /**
-         * On Connected change Login Bar
-         */
-        client.on('connected', function () {
-            $('.login').hide();
-            $('.connected').show();
-        });
-
-        /**
-         *  Session Started Handling
-         */
-        client.on('session:started', function () {
-            client.subscribe("xmppftw@localhost");
-            client.getRoster(function (err, response) {
-                client.updateCaps();
-                client.sendPresence({
-                    caps: client.disco.caps
-                });
-                if (err == null) {
-                    console.dir(response);
-                    var ownJid = response.to.bare;
-                    var roster = response.roster.items;
-                    var friends = [];
-                    $.each(roster, function(index, item){
-                        var friend = {};
-                        friend.jid = item.jid.bare;
-                        friend.local = item.jid.local;
-                        friend.subscription = item.subscription;
-                        friend.subscriptionRequested = item.subscriptionRequested;
-                        friends.push();
-                    });
-                    fillUserList(ownJid);
-                }
+    client.on('session:started', function () {
+        client.subscribe("xmppftw@localhost");
+        client.getRoster(function (err, response) {
+            client.updateCaps();
+            client.sendPresence({
+                caps: client.disco.caps
             });
-        });
-
-        /**
-         * Send Message and print to chat
-         */
-        client.on('message', function (message) {
-            console.log(message);
-            $('.chat-messages').append("<li class=\"chat-message own\">" + message + "</li>");
-        });
-
-        /**
-         * On Message sent
-         */
-        client.on('message:sent', function (message) {
-            console.log(message);
-        });
-
-        /**
-         * On Chat partner is composing
-         */
-        client.on('chat:state', function () {
-
-        });
-
-        /**
-         * On Message received
-         */
-        client.on('chat', function (message) {
-
-        });
-
-        /**
-         * Log of all xmpp client events
-         */
-        client.on('*', function (name) {
-            if (debugMode) {
-                console.log(name);
+            if (err == null) {
+                console.dir(response);
+                var ownJid = response.to.bare;
+                var roster = response.roster.items;
+                var friends = [];
+                $.each(roster, function (index, item) {
+                    var friend = {};
+                    friend.jid = item.jid.bare;
+                    friend.local = item.jid.local;
+                    friend.subscription = item.subscription;
+                    friend.subscriptionRequested = item.subscriptionRequested;
+                    friends.push();
+                });
+                fillUserList(ownJid);
             }
         });
-    }
-});
+    });
+
+    /**
+     * Send Message and print to chat
+     */
+    client.on('message', function (message) {
+
+    });
+
+    /**
+     * On Message sent
+     */
+    client.on('message:sent', function (message) {
+        console.log("message sent");
+        addChatMessage(message, true);
+    });
+
+    /**
+     * On Chat partner is composing
+     */
+    client.on('chat:state', function () {
+
+    });
+
+    /**
+     * On Message received
+     */
+    client.on('chat', function (message) {
+        addChatMessage(message, false);
+    });
+
+    /**
+     * Log of all xmpp client events
+     */
+    client.on('*', function (name) {
+        if (debugMode) {
+            console.log(name);
+        }
+    });
+};
